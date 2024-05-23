73c44ea624595e03279489e8f6eb4250537d88ae
Mohan4656 <mohanprasath4656@gmail.com>
2024-05-23 17:01:07 +0530

    Base code

---
+++ src/test/java/Gitworks/Pushandpull.java
@@ -0,0 +1,21 @@
+package Gitworks;
+
+public class Pushandpull {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Mohan Have a vibe");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
